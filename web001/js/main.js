bfc496021a5b26a92274a3f6725db82b1a15bd8a
Ryan Supak <ryansupak@gmail.com>
2021-08-25 23:12:11 -0500

    livecodingnow

--- web001/js/main.js
+++ web001/js/main.js
@@ -1,58 +1,111 @@
-var SamplePlayer_BD = {};
-var SamplePlayer_CH = {};
-var SamplePlayer_SN = {};
+var DrumSampler = {};
+var DrumPlayers = {};
 
 function StartAudioEngine(event)
 {
+
 	Tone.start();
 	console.log("audio engine started!");
-	console.log(Tone);
-
-	console.log("creating sample players...");
-	SamplePlayer_BD = new Tone.Player("../wav/BD-001.wav");
-	SamplePlayer_CH = new Tone.Player("../wav/CH-001.wav");
-	SamplePlayer_SN = new Tone.Player("../wav/SN-001.wav");
-
-	console.log(SamplePlayer_BD);
-	console.log(SamplePlayer_CH);
-	console.log(SamplePlayer_SN);
-
-	var MultibandCompressor_Master = new Tone.MultibandCompressor
-	({
-		lowFrequency: 200,
-		highFrequency: 2000,
-		low:{
-				threshold: -9
-			},
-		mid:{
-				threshold: -12
-			},
-		high:{
-				threshold: -9
-			}
+	
+	
+	DrumSampler = new Tone.Sampler({
+	urls:
+		{
+			A1: "../wav/BD-001.wav",
+			A2: "../wav/SN-001.wav",
+			A3: "../wav/CH-001.wav",
+		}
 	}).toDestination();
+	console.log("sampler created!");
+	console.log(DrumSampler);
+	
 
-	SamplePlayer_BD.connect(MultibandCompressor_Master);
-	SamplePlayer_CH.connect(MultibandCompressor_Master);
-	SamplePlayer_SN.connect(MultibandCompressor_Master);
+	DrumPlayers = new Tone.Players({
+	urls:
+		{
+			"BD": "../wav/BD-001.wav",
+			"SN": "../wav/SN-001.wav",
+			"CH": "../wav/CH-001.wav",
+		}
+	}).toDestination();
+	console.log("players created!");
+	console.log(DrumPlayers);
+
+
 }
 
 function PlayAudio(event)
 {
-	var BD_Loop = new Tone.Loop(time =>
-	{
-		SamplePlayer_BD.start();
-	}, "4n").start(0);
 
-	var SN_Loop = new Tone.Loop(time =>
-	{
-		SamplePlayer_SN.start();
-	}, "2n").start("+4n");
+	
 
-	var CH_Loop = new Tone.Loop(time =>
-	{
-		SamplePlayer_CH.start();
-	}, "16n").start(0);
+	Tone.Transport.schedule((time) => {DrumSampler.triggerAttack("A2",time,0.75); },"0:3:0");
+
+	Tone.Transport.schedule((time) => {DrumSampler.triggerAttack("A3",time,0.7); },"0:0:0");
+	Tone.Transport.schedule((time) => {DrumSampler.triggerAttack("A3",time,0.5); },"0:0:1");
+	Tone.Transport.schedule((time) => {DrumSampler.triggerAttack("A3",time,0.3); },"0:0:2");
+	Tone.Transport.schedule((time) => {DrumSampler.triggerAttack("A3",time,0.1); },"0:0:3");
+
+	Tone.Transport.schedule((time) => {DrumSampler.triggerAttack("A3",time,0.7); },"0:0:4");
+	Tone.Transport.schedule((time) => {DrumSampler.triggerAttack("A3",time,0.5); },"0:0:5");
+	Tone.Transport.schedule((time) => {DrumSampler.triggerAttack("A3",time,0.3); },"0:0:6");
+	Tone.Transport.schedule((time) => {DrumSampler.triggerAttack("A3",time,0.1); },"0:0:7");
+
+	Tone.Transport.schedule((time) => {DrumSampler.triggerAttack("A3",time,0.7); },"0:0:8");
+	Tone.Transport.schedule((time) => {DrumSampler.triggerAttack("A3",time,0.5); },"0:0:9");
+	Tone.Transport.schedule((time) => {DrumSampler.triggerAttack("A3",time,0.3); },"0:0:10");
+	Tone.Transport.schedule((time) => {DrumSampler.triggerAttack("A3",time,0.1); },"0:0:11");
+
+	Tone.Transport.schedule((time) => {DrumSampler.triggerAttack("A3",time,0.7); },"0:0:12");
+	Tone.Transport.schedule((time) => {DrumSampler.triggerAttack("A3",time,0.5); },"0:0:13");
+	Tone.Transport.schedule((time) => {DrumSampler.triggerAttack("A3",time,0.3); },"0:0:14");
+	Tone.Transport.schedule((time) => {DrumSampler.triggerAttack("A3",time,0.1); },"0:0:15");
+
+	Tone.Transport.schedule((time) => {DrumSampler.triggerAttack("A1",time,0.3); },"0:0:15");
+	
+
+	
+	Tone.Transport.schedule((time) => {DrumSampler.triggerAttack("A1",time,1); },"0:0:0");
+	Tone.Transport.schedule((time) => {DrumSampler.triggerAttack("A1",time,0.75); },"0:1:0");
+	Tone.Transport.schedule((time) => {DrumSampler.triggerAttack("A1",time,1); },"0:2:0");
+	Tone.Transport.schedule((time) => {DrumSampler.triggerAttack("A1",time,0.75); },"0:3:0");
+
+	Tone.Transport.schedule((time) => {DrumSampler.triggerAttack("A2",time,0.75); },"0:1:0");
+	Tone.Transport.schedule((time) => {DrumSampler.triggerAttack("A2",time,0.15); },"0:1:1");
+	Tone.Transport.schedule((time) => {DrumSampler.triggerAttack("A2",time,0.05); },"0:1:2.1");
+	Tone.Transport.schedule((time) => {DrumSampler.triggerAttack("A2",time,0.02); },"0:1:3.2");
+
+	Tone.Transport.schedule((time) => {DrumSampler.triggerAttack("A2",time,0.75); },"0:3:0");
+
+	Tone.Transport.schedule((time) => {DrumSampler.triggerAttack("A3",time,0.7); },"0:0:0");
+	Tone.Transport.schedule((time) => {DrumSampler.triggerAttack("A3",time,0.5); },"0:0:1");
+	Tone.Transport.schedule((time) => {DrumSampler.triggerAttack("A3",time,0.3); },"0:0:2");
+	Tone.Transport.schedule((time) => {DrumSampler.triggerAttack("A3",time,0.1); },"0:0:3");
+
+	Tone.Transport.schedule((time) => {DrumSampler.triggerAttack("A3",time,0.7); },"0:0:4");
+	Tone.Transport.schedule((time) => {DrumSampler.triggerAttack("A3",time,0.5); },"0:0:5");
+	Tone.Transport.schedule((time) => {DrumSampler.triggerAttack("A3",time,0.3); },"0:0:6");
+	Tone.Transport.schedule((time) => {DrumSampler.triggerAttack("A3",time,0.1); },"0:0:7");
+
+	Tone.Transport.schedule((time) => {DrumSampler.triggerAttack("A3",time,0.7); },"0:0:8");
+	Tone.Transport.schedule((time) => {DrumSampler.triggerAttack("A3",time,0.5); },"0:0:9");
+	Tone.Transport.schedule((time) => {DrumSampler.triggerAttack("A3",time,0.3); },"0:0:10");
+	Tone.Transport.schedule((time) => {DrumSampler.triggerAttack("A3",time,0.1); },"0:0:11");
+
+	Tone.Transport.schedule((time) => {DrumSampler.triggerAttack("A3",time,0.7); },"0:0:12");
+	Tone.Transport.schedule((time) => {DrumSampler.triggerAttack("A3",time,0.5); },"0:0:13");
+	Tone.Transport.schedule((time) => {DrumSampler.triggerAttack("A3",time,0.3); },"0:0:14");
+	Tone.Transport.schedule((time) => {DrumSampler.triggerAttack("A3",time,0.1); },"0:0:15");
+
+	Tone.Transport.schedule((time) => {DrumSampler.triggerAttack("A1",time,0.3); },"0:0:15");
+	
+
+	Tone.Transport.setLoopPoints("0:0:0", "1:0:0");
+	Tone.Transport.loop = true;
+
+	Tone.Transport.swingSubdivision = "16n";
+	Tone.Transport.swing = 0.15;
+	Tone.Transport.bpm.value = 75;
 
 	Tone.Transport.start();
 }
